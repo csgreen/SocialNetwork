3986a244e2dbd4ea5d579fe26479c0f3cb8747e7
Christina Green <csgreen@gmail.com>
2013-10-21 20:47:06 -0700

    Initial Commit

---
+++ SocialNetwork/CSGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CSGAppDelegate.h
+//  SocialNetwork
+//
+//  Created by Christina Green on 10/21/13.
+//  Copyright (c) 2013 Greenster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SocialNetwork/CSGViewController.h
@@ -0,0 +1,13 @@
+//
+//  CSGViewController.h
+//  SocialNetwork
+//
+//  Created by Christina Green on 10/21/13.
+//  Copyright (c) 2013 Greenster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSGViewController : UIViewController
+
+@end
